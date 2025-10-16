d26c1295fc01a401596ff6391f23170c9b48c717
Muhammad Hasnain <hasnainalamglt143@gmail.com>
2025-10-16 21:19:34 +0500

    Fix some settings

--- backend/ecomm_config/urls.py
+++ backend/ecomm_config/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('owner-boss/', admin.site.urls),
     path('cart/', include('cart.urls')),
     path('payment/', include('payment.urls')),
     path('', include('store.urls')),
