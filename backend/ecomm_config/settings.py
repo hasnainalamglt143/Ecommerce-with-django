0a0179409697f69fa09cc7e982273c83708b5a50
Muhammad Hasnain <hasnainalamglt143@gmail.com>
2025-10-16 19:25:04 +0500

    added whitenoise middleware

--- backend/ecomm_config/settings.py
+++ backend/ecomm_config/settings.py
@@ -63,7 +63,9 @@ if DEBUG:
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # ✅ Add here
     'django.middleware.common.CommonMiddleware',
+    
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
